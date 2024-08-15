c2a225252bbe8f1c07d33234c3dc45ea18f6c2eb
yesh <yeshchoudhari68@gmail.com>
2024-08-15 16:02:18 +0530

    add function to check user give input or not

--- loginPage/script.js
+++ loginPage/script.js
@@ -22,3 +22,28 @@ buttonElement2.addEventListener('click', function () {
 
     console.log(inputValue);
 });
+
+
+document.getElementById('myForm').addEventListener("submit",function(event){
+    const emailinput = document.getElementById("Email");
+    const errorspan = document.getElementById("error");
+
+    if(emailinput.value.trim()==""){
+        errorspan.style.display = "inline";
+        event.preventDefault();
+    }else{
+        errorspan.style.display="none";
+    }
+});
+
+document.getElementById('myForm').addEventListener("submit",function(event){
+    const emailinput = document.getElementById("Password");
+    const errorspan = document.getElementById("errorPassword");
+
+    if(emailinput.value.trim()==""){
+        errorspan.style.display = "inline";
+        event.preventDefault();
+    }else{
+        errorspan.style.display="none";
+    }
+});
