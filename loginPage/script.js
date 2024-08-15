ff6594bbd65ca8880cc8c1fd58655e8eac2c88a0
yesh <yeshchoudhari68@gmail.com>
2024-08-15 13:07:00 +0530

    Login page

---
+++ loginPage/script.js
@@ -0,0 +1,24 @@
+
+let buttonElement1 = document.getElementById('myButton');
+
+
+buttonElement1.addEventListener('click', function () {
+    
+    let inputElement = document.getElementById('Email');
+    
+    let inputValue = inputElement.value;
+    
+    console.log(inputValue);
+});
+
+let buttonElement2 = document.getElementById('myButton');
+
+
+buttonElement2.addEventListener('click', function () {
+
+    let inputElement = document.getElementById('Password');
+
+    let inputValue = inputElement.value;
+
+    console.log(inputValue);
+});
